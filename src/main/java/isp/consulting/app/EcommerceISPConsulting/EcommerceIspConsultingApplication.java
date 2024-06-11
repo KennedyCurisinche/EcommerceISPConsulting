59245c5ddd7671f0c21c4abfa2ed0a67cf0a0a64
KennedyCurisinche <rogger9081@gmail.com>
2024-06-11 10:21:29 -0500

    .init

---
+++ src/main/java/isp/consulting/app/EcommerceISPConsulting/EcommerceIspConsultingApplication.java
@@ -0,0 +1,13 @@
+package isp.consulting.app.EcommerceISPConsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceIspConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceIspConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isp/consulting/app/EcommerceISPConsulting/EcommerceIspConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package isp.consulting.app.EcommerceISPConsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceIspConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
